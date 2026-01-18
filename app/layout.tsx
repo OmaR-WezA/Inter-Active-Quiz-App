9d12353655d62362bcd9b834458ddfc4af002d67
Omar-WezA <omarfahem11@gmail.com>
2026-01-18 13:37:51 +0200

    Updated project

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,21 +11,7 @@ export const metadata: Metadata = {
   description: 'Created with Weza Production',
   generator: 'Omar Fahem',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+
   },
 }
 
